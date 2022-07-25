bfdc15bf290d9301ac072634aa92de3eeb747821
Timothy Pew <mrlasers@mrlasers.com>
2022-07-25 02:16:02 -0700

    rebased with split webpack

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,46 +3,192 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
+import List
+import Random
+import Uuid
+
+
+type alias Todo =
+    { id : String, title : String, description : String }
+
+
+type alias FormData =
+    { title : String, description : String }
+
+
+type ListStyle
+    = TodoGrid
+    | TodoList
+
+
+listStyleToClass : ListStyle -> String
+listStyleToClass style =
+    case style of
+        TodoGrid ->
+            "grid"
+
+        TodoList ->
+            "list"
+
+
+type alias Model =
+    { seed : Random.Seed
+    , todos : List Todo
+    , displayStyle : ListStyle
+    , form : FormData
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = GotNewSeed Random.Seed
+    | AddTodo
+    | NewFormTitle String
+    | NewFormDescription String
+    | SetDisplayType ListStyle
+    | ResetForm
+    | DeleteAllTodos
+
+
+newUuid : Random.Seed -> ( String, Random.Seed )
+newUuid seed =
+    seed |> Random.step Uuid.uuidGenerator |> Tuple.mapFirst Uuid.toString
+
+
+
+-- FORM HELPER FUNCTIONS
+
+
+emptyForm : FormData
+emptyForm =
+    { title = ""
+    , description = ""
+    }
+
+
+updateFormTitle : String -> FormData -> FormData
+updateFormTitle title form =
+    { form | title = title }
+
+
+updateFormDescription : String -> FormData -> FormData
+updateFormDescription description form =
+    { form | description = description }
+>>>>>>> d94ca4a (major functionality seems to be in place)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        GotNewSeed newSeed ->
+            ( { model | seed = newSeed }, Cmd.none )
+
+        NewFormTitle title ->
+            ( { model | form = model.form |> updateFormTitle title }, Cmd.none )
+
+        NewFormDescription description ->
+            ( { model | form = model.form |> updateFormDescription description }, Cmd.none )
+
+        SetDisplayType style ->
+            case style of
+                TodoGrid ->
+                    ( { model | displayStyle = TodoGrid }, Cmd.none )
+
+                TodoList ->
+                    ( { model | displayStyle = TodoList }, Cmd.none )
+
+        AddTodo ->
+            if String.isEmpty model.form.title then
+                ( model, Cmd.none )
+
+            else
+                let
+                    ( id, seed ) =
+                        newUuid model.seed
+
+                    { todos } =
+                        model
+
+                    { title, description } =
+                        model.form
+                in
+                update ResetForm { model | todos = todos ++ [ { id = id, title = title, description = description } ] }
+
+        ResetForm ->
+            ( { model | form = emptyForm }, generateNewSeed )
+
+        DeleteAllTodos ->
+            ( { model | todos = [] }, Cmd.none )
 
 
-type alias Model =
-    Int
-
-
-initialModel : Model
-initialModel =
-    0
-
+generateNewSeed : Cmd Msg
+generateNewSeed =
+    Random.generate GotNewSeed Random.independentSeed
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Hello, World!" ]
-        , div [ class "counter" ]
-            [ div [] [ text (String.fromInt model) ]
-            , button [ onClick Decrement ] [ text "-" ]
-            , button [ onClick Increment ] [ text "+" ]
+
+    div [ class "container" ]
+        [ header []
+            [ nav []
+                [ button [ onClick (SetDisplayType TodoGrid) ] [ text "Grid" ]
+                , button [ onClick (SetDisplayType TodoList) ] [ text "List" ]
+                , button [ onClick DeleteAllTodos ] [ text "Delete All" ]
+                ]
             ]
+        , main_ []
+            [ Html.form [ onSubmit AddTodo ]
+                [ label []
+                    [ span [] [ text "Title" ]
+                    , viewInput "text" "Todo title" model.form.title NewFormTitle
+                    ]
+                , label []
+                    [ span [] [ text "Description" ]
+                    , viewInput "textarea" "Todo description/notes" model.form.description NewFormDescription
+                    ]
+                , input [ type_ "submit", value "Add" ] []
+                ]
+            , div [ class "todo-list" ]
+                (if List.isEmpty model.todos then
+                    [ text "Add some todos" ]
+
+                 else
+                    [ ul [ class ("todo-list " ++ listStyleToClass model.displayStyle) ]
+                        (List.map viewTodoItem <| model.todos)
+                    ]
+                )
+
+viewTodoItem : Todo -> Html msg
+viewTodoItem { title, id, description } =
+    li [ class "todo-item" ]
+        [ h3 [] [ text title ]
+        , p [] [ text description ]
+        , div [ class "id" ] [ text id ]
         ]
 
 
-main : Program () Model Msg
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    case t of
+        "textarea" ->
+            textarea [ placeholder p, value v, onInput toMsg ] []
+
+        _ ->
+            input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+type alias Flags =
+    { seed : Int }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init { seed } =
+    ( { seed = Random.initialSeed seed, todos = [], form = { title = "", description = "" }, displayStyle = TodoList }, generateNewSeed )
+
+
+main : Program Flags Model Msg
+>>>>>>> d94ca4a (major functionality seems to be in place)
 main =
     Browser.element
         { init = \flags -> ( initialModel, Cmd.none )
--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,11 @@
-const { Elm } = require("../elm/Main.elm");
+const { Elm } = require('../elm/Main.elm')
 
-Elm.Main.init({ node: document.getElementById("app") });
+const getRandomInts = (n) => {
+  const randInts = new Uint32Array(n)
+  crypto.getRandomValues(randInts)
+  return Array.from(randInts)
+}
+
+const flags = { seed: getRandomInts(5)[0] }
+
+Elm.Main.init({ node: document.getElementById('app'), flags })
